a508973503a5477e3b5a023af76eb6b9bf7a6346
wuxianye <164884853+wuxianye@users.noreply.github.com>
2024-05-04 14:03:51 +0100

    Add files via upload

---
+++ task_e.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(int argc, char* argv[])
+{
+	// Enter your code under here to read the filename from the command line
+	if (argc != 2) {
+		return 1; 
+	}
+	// Read number from file - do not edit
+	double num;
+	FILE* fptr;
+	fptr = fopen(argv[1], "r");
+	fscanf(fptr, "%lf", &num);
+	fclose(fptr);
+
+	// Enter your code from Lab D to print Json output (with name, email, number1, number2) goes under here
+	
+	char* name = "xianye wu";
+	char* email = "xianye.wu@student.manchester.ac.uk";
+
+	// Output JSON formatted text
+	printf("{\"name\":\"%s\",\"email\":\"%s\",\"number1\":%.2f,\"number2\":%.3e}\n", name, email, num, num);
+	
+	// Do not edit below here
+
+
+	return 0;
+}
