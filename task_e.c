b5fe009da1124243a58c70e911827e3484390c85
wuxianye <164884853+wuxianye@users.noreply.github.com>
2024-05-04 14:22:15 +0100

    Add files via upload

--- task_e.c
+++ task_e.c
@@ -3,7 +3,7 @@ int main(int argc, char* argv[])
 {
 	// Enter your code under here to read the filename from the command line
 	if (argc != 2) {
-		printf("Error.Expected 1 filename.");
+		printf("Error. Expected 1 filename.");
 		return 1; 
 	}
 	// Read number from file - do not edit
@@ -16,7 +16,7 @@ int main(int argc, char* argv[])
 	// Enter your code from Lab D to print Json output (with name, email, number1, number2) goes under here
 	
 	char* name = "xianye wu";
-	char* email = "xianye.wu.@student.manchester.ac.uk";
+	char* email = "xianye.wu@student.manchester.ac.uk";
 
 	// Output JSON formatted text
 	printf("{\"name\":\"%s\",\"email\":\"%s\",\"number1\":%.2f,\"number2\":%.3e}\n", name, email, num, num);
